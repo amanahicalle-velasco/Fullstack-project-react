d0bd86837c24f8dba0aa62a778f8a0ff0dddb55a
Anahi Mariel Calle Velasco <am.anahicalle@gmail.com>
2026-04-17 22:32:32 -0400

    subiendo mens

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,44 @@
-
 require("dotenv").config();
 
 const express = require("express");
-
 const cors= require("cors");
-
 const { PrismaClient } = require("@prisma/client");
 const { PrismaPg } = require("@prisma/adapter-pg");
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = "mi_clave_secreta";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 const prisma = new PrismaClient ({ adapter });
-
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-/*
-let tasks = [
-  { id: 1, title: 'Study Express', completed: false },
-  { id: 2, title: 'Build backend', completed: true },
-];
-*/
+const verifyToken = (req: any, res: any, next: any) => {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
+    return res.status(403).json({ error: "Token requerido" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Token inválido" });
+  }
+};
+
+app.get("/private", verifyToken, (req: any, res: any) => {
+  res.json({ message: "Acceso permitido" });
+});
+
 app.get("/", (req:any, res:any) => {
     res.send("Backend is running");
 });
@@ -38,28 +52,26 @@ app.get("/tasks",async (req:any, res:any) => {
     res.status(500).json({ error: "Error al obtener tareas" });
   }
 });
+app.post("/login", (req: any, res: any) => {
+  const { username, password } = req.body;
+  
+  if (!username || !password) {
+    return res.status(400).json({ error: "Faltan datos" });
+  }
 
-/*
-app.get ("/tasks", (req:any , res:any) => {
-    res.json (tasks);
+  if (username === "admin" && password === "1234") {
+
+    const token = jwt.sign(
+      { username: username },
+      SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
+    return res.json({ token });
+  }
+
+  res.status(401).json({ error: "Credenciales inválidas" });
 });
-
-app.post("/tasks", (req:any , res:any) => {
-
-    console.log("POST /tasks fue llamado");
-    
-    console.log("Datos recibidos", req.body);
-
-
-    const newTask = {
-        id: req.body.id,
-        title: req.body.title,
-        completed: req.body.completed
-    };
-    tasks.push(newTask);
-    console.log("Lista actualizada:", tasks);
-    res.json(newTask);
-});*/
 
 app.post("/tasks", async (req:any, res:any) => {
   try {
@@ -77,28 +89,6 @@ app.post("/tasks", async (req:any, res:any) => {
   }
 });
 
-/*
-
-app.delete("/tasks/:id", (req:any, res:any) => {
-  const id = Number(req.params.id);
-
-  tasks = tasks.filter(task => task.id !== id);
-
-  res.json({ message: "Tarea eliminada" });
-});
-
-app.put("/tasks/:id", (req:any, res:any) => {
-  const id = Number(req.params.id);
-
-  tasks = tasks.map(task =>
-    task.id === id
-      ? { ...task, completed: !task.completed }
-      : task
-  );
-
-  res.json({ message: "Tarea actualizada" });
-});
-*/
 app.put("/tasks/:id", async (req:any, res:any) => {
   try {
   const TaskId = Number(req.params.id);
@@ -129,6 +119,8 @@ app.delete("/tasks/:id", async (req:any, res:any) => {
     res.status(500).json({ error: "Error al eliminar tarea" });
   }
 });
+
+
 
 app.listen (PORT, () => {
     console.log (`Server is running on port ${PORT}`);
