ad85df7240007bc00052c43573db02dca77bb504
Anahi Mariel Calle Velasco <am.anahicalle@gmail.com>
2026-07-14 21:01:14 -0400

    fix: corregir caso culpable, todos los checks en verde

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -46,7 +46,7 @@ test("un usuario puede crear una tarea y verla en la lista", async ({ page }) =>
     .click();
 
   await expect(
-  page.getByText("ESTA TAREA NO EXISTE").first()
+  page.getByText(nombre).first()
 ).toBeVisible();
 
 });
